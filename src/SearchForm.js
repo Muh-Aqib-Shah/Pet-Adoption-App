66f8fe73ed210b1f14ef385e0eaafe307ca9aa75
Aqib <aqibshah1310@gmail.com>
2023-07-24 01:40:48 +0500

    Added Drawer Effect

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,23 +1,39 @@
-export const SearchForm = () => {
-     
+import Logo2 from './images/petshop_logo (2).png';
+
+export const SearchForm = () => {     
     const ANIMALS = ['Bird','Cat','Dog','Rabbit','Reptile']
     return(
     <div className="srch-container">
         <div className="form-container">
+            <div className='message-block'>
+           <img className="srch-logo" src={Logo2} />
+           <div className="srch-msg">Search for Pets 
+           <br />
+           in Your Local Area</div>
+           </div>
+           <hr /> 
             <form>
                 <div className="loct-box">
-                <label>Location<input type="select"></input></label>
+                <label>Location
+                    <br />
+                <input type="select"></input></label>
                 </div>
                 <div className="animal-box">
-                <label>Animals
+                <label>Animal
+                    <br />
                     <select>
                         <option />
-                        {ANIMALS.map(item => (<option>item</option>))}
+                        {ANIMALS.map(item => (<option>{item}</option>))}
                     </select>
                 </label>
                 </div>
                 <div className="breed-box">
-                <label>Breed<input type="select"></input></label>
+                <label>Breed
+                    <br />
+                    <input type="select"></input></label>
+                </div>
+                <div className='submit-btn'>
+                    <input className='sub-btn' type='submit' value="Go!"/>
                 </div>
             </form>
         </div>
